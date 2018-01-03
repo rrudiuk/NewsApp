5523a8c7f88f97b13b8a329012a6c778170bfac2
ruslan <rus.barik@gmail.com>
2018-01-03 16:17:14 +0200

    public class news and oblect constructor

---
+++ app/src/main/java/com/example/android/newsapp/News.java
@@ -0,0 +1,58 @@
+package com.example.android.newsapp;
+
+/**
+ * Created by ruslan on 03.01.18.
+ */
+
+public class News {
+
+    private String sourceName;
+    private String title;
+    private String urlToImage;
+    private String date;
+    private String description;
+    private String url;
+    private String author;
+
+    public News(String sourceName, String title, String urlToImage, String date, String description, String url, String author) {
+
+        this.sourceName = sourceName;
+        this.title = title;
+        this.urlToImage = urlToImage;
+        this.date = date;
+        this.description = description;
+        this.url = url;
+        this.author = author;
+
+    }
+
+    // getters
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
