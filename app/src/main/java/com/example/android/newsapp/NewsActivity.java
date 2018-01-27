18992646aa87d2c86ee3303b03448daf8377ebf5
ruslan <rus.barik@gmail.com>
2018-01-27 19:23:04 +0200

    Added a progress bar, an empty view and a connectivity verification

--- app/src/main/java/com/example/android/newsapp/NewsActivity.java
+++ app/src/main/java/com/example/android/newsapp/NewsActivity.java
@@ -1,11 +1,17 @@
 package com.example.android.newsapp;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +30,9 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
      * This really only comes into play if you're using multiple loaders.
      */
     private static final int NEWS_LOADER_ID = 1;
+    /** TextView that is displayed when the list is empty */
+    private TextView mEmptyStateTextView;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,19 +42,41 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
         // Find a reference to the {@link ListView} in the layout
         ListView newsListView = (ListView) findViewById(R.id.list);
 
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+        // Fing a view with no results found
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        newsListView.setEmptyView(mEmptyStateTextView);
+
         // Create a new adapter that takes an empty list of news as input
         mAdapter = new NewsAdapter(this, new ArrayList<News>());
 
-        // Get a reference to the LoaderManager, in order to interact with loaders.
-        LoaderManager loaderManager = getLoaderManager();
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(NEWS_LOADER_ID, null, this);
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
-        newsListView.setAdapter(mAdapter);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        if (activeNetwork != null && activeNetwork.isConnected()) {
+
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+            // Set the adapter on the {@link ListView}
+            // so the list can be populated in the user interface
+            newsListView.setAdapter(mAdapter);
+
+        }   else {
+
+            // Hide progress bar
+            mProgressBar.setVisibility(View.INVISIBLE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText("No internet connection");
+        }
     }
 
     @Override
@@ -57,6 +88,10 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
 
+        // Hide progress bar
+        mProgressBar.setVisibility(View.INVISIBLE);
+        // // Set empty state text to display "No news found"
+        mEmptyStateTextView.setText("No news found...");
         // Clear the adapter of previous news data
         mAdapter.clear();
 
